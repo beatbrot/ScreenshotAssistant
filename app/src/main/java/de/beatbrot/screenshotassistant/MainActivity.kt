94080e03f9e89786407d1d4bde720290f0f705c6
Christoph Loy <loy.christoph@gmail.com>
2019-08-05 20:58:38 +0200

    Add options button

--- app/src/main/java/de/beatbrot/screenshotassistant/MainActivity.kt
+++ app/src/main/java/de/beatbrot/screenshotassistant/MainActivity.kt
@@ -1,13 +1,23 @@
 package de.beatbrot.screenshotassistant
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.graphics.Rect
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewAnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.hypot
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: ScreenshotActivityViewModel
@@ -30,7 +40,19 @@ class MainActivity : AppCompatActivity() {
             view.cropRect = Rect(view.wholeImageRect)
         }
 
+        menuButton.setOnClickListener {
+            val popMenu = PopupMenu(baseContext, it)
+            popMenu.menuInflater.inflate(R.menu.about_menu, popMenu.menu)
+            popMenu.show()
+        }
+
         button.setOnClickListener { viewModel.shareImage(screenShot.croppedImage) }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = MenuInflater(baseContext)
+        inflater.inflate(R.menu.about_menu, menu)
+        return true
     }
 
     private fun initViewModel() {
